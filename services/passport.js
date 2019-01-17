7a0657e110024aab087696810c0061603abde210
bakalux <swaveful@gmail.com>
2019-01-17 05:21:03 +0300

    finished google Oauth and implementing saving user in mongoDB

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,20 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const passport = require('passport');
 
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+})
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
@@ -9,10 +22,18 @@ passport.use(new GoogleStrategy({
   callbackURL: '/auth/google/callback'
 },
   (accessToken, refreshToken, profile, done) => {
-    console.dir({
-      "accessToken": accessToken,
-      "refreshToken": refreshToken,
-      "profile": profile
-    })
+    User.findOne({ googleID: profile.id })
+      .then(existingUser => {
+        if(existingUser) {
+          // We already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          // We don't have a user record with this ID, make a new record
+          new User({ googleID: profile.id }).save()
+            .then(user => {
+              done(null, user);
+            })
+        }
+      });
   }
 ));
